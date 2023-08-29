bd858b0fc71f4dbcfccb1630afeca5d190f1ceb9
Ayushi Rastogi <ayrastog@amazon.com>
2023-08-29 06:02:20 +0530

    Adding event type.

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+//POJO to store event
+public class Event {
+    String userID;
+    String payload;
+    public String getUserID() {
+        return userID;
+    }
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    public String getPayload() {
+        return payload;
+    }
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+}
